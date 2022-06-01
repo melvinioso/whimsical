e92ffcfe22425b389f87b039975dbf9d22e59f49
Matt McGavern <matt.mcgavern@gmail.com>
2022-06-01 17:52:35 -0500

    RadioButton wip

---
+++ react-components/src/components/Inputs/RadioButton/index.tsx
@@ -0,0 +1,209 @@
+import React, { Children, isValidElement, cloneElement } from 'react';
+import styled from 'styled-components';
+import clsx from 'clsx';
+
+import Error from '../Error';
+
+export function Option({ groupName, name, value, register }: any) {
+  return (
+    <div>
+      <input name={groupName} type="radio" value={value} ref={register} />
+      <span></span>
+      <label className="option-label">{name}</label>
+    </div>
+  );
+}
+
+function _RadioButton({
+  className,
+  name,
+  label,
+  small,
+  large,
+  register,
+  children,
+  error,
+}: any) {
+  // if (!register) {
+  //   return null;
+  // }
+
+  const childrenOverride = Children.map(children, (child) => {
+    if (!isValidElement(child)) {
+      return child;
+    }
+
+    return cloneElement(child as React.ReactElement<any>, {
+      groupName: name,
+      register,
+    });
+  });
+
+  return (
+    <>
+      <div className={clsx(className, small && 'small', large && 'large')}>
+        <label>{label}</label>
+        <div>{childrenOverride}</div>
+      </div>
+      {error ? (
+        <Error red marginT-7 marginL-5>
+          {error.message}
+        </Error>
+      ) : null}
+    </>
+  );
+}
+
+const RadioButton = styled(_RadioButton)`
+  font-family: 'Assistant', sans-serif;
+  display: flex;
+  flex-direction: column;
+
+  > label {
+    min-width: 100px;
+    font-weight: 600;
+  }
+
+  > div > div {
+    margin: 8px 0;
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    input {
+      margin-right: 20px;
+      z-index: 9999;
+      opacity: 0;
+      cursor: pointer;
+      width: 16px;
+      height: 16px;
+    }
+
+    span {
+      position: absolute;
+      width: 16px;
+      height: 16px;
+      top: 2px;
+      left: 2px;
+      border: solid 2px ${({ theme }) => theme.colors.whimsical.smoke};
+      border-radius: 50%;
+    }
+
+    input:checked ~ span {
+      background-color: ${({ theme }) => theme.colors.whimsical.indigo};
+      border-color: ${({ theme }) => theme.colors.whimsical.indigo};
+
+      &:after {
+        content: '';
+        position: absolute;
+        border: solid 2px white;
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        top: 1px;
+        left: 1px;
+      }
+    }
+  }
+
+  &.small {
+    > label {
+      font-size: 0.7rem;
+    }
+
+    .option-label {
+      font-size: 0.8rem;
+    }
+
+    > div > div {
+      margin: 8px 0;
+      display: flex;
+      align-items: center;
+      position: relative;
+  
+      input {
+        margin-right: 20px;
+        z-index: 9999;
+        opacity: 0;
+        cursor: pointer;
+        width: 12px;
+        height: 12px;
+      }
+  
+      span {
+        position: absolute;
+        width: 12px;
+        height: 12px;
+        top: 2px;
+        left: 2px;
+        border: solid 2px ${({ theme }) => theme.colors.whimsical.smoke};
+        border-radius: 50%;
+      }
+  
+      input:checked ~ span {
+        background-color: ${({ theme }) => theme.colors.whimsical.indigo};
+        border-color: ${({ theme }) => theme.colors.whimsical.indigo};
+  
+        &:after {
+          content: '';
+          position: absolute;
+          border: solid 2px white;
+          width: 6px;
+          height: 6px;
+          border-radius: 50%;
+          top: 1px;
+          left: 1px;
+        }
+      }
+    }
+  }
+
+  &.large {
+    .option-label {
+      font-size: 1.2rem;
+    }
+
+    > div > div {
+      margin: 8px 0;
+      display: flex;
+      align-items: center;
+      position: relative;
+  
+      input {
+        margin-right: 20px;
+        z-index: 9999;
+        opacity: 0;
+        cursor: pointer;
+        width: 20px;
+        height: 20px;
+      }
+  
+      span {
+        position: absolute;
+        width: 20px;
+        height: 20px;
+        top: 2px;
+        left: 2px;
+        border: solid 2px ${({ theme }) => theme.colors.whimsical.smoke};
+        border-radius: 50%;
+      }
+  
+      input:checked ~ span {
+        background-color: ${({ theme }) => theme.colors.whimsical.indigo};
+        border-color: ${({ theme }) => theme.colors.whimsical.indigo};
+  
+        &:after {
+          content: '';
+          position: absolute;
+          border: solid 3px white;
+          width: 14px;
+          height: 14px;
+          border-radius: 50%;
+          top: 0px;
+          left: 0px;
+        }
+      }
+    }
+`;
+
+export default RadioButton;
---
+++ react-components/src/stories/RadioButton/RadioButton.stories.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { get } from 'lodash';
+
+import RadioButton, { Option } from '../../components/Inputs/RadioButton';
+import Button from '../../components/Button';
+
+export default { title: 'Whimsical Components/Inputs/Radio Button' };
+
+const Container = styled.div`
+  width: 380px;
+`;
+
+const Spacer = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const Default = () => (
+  <Container>
+    <RadioButton name="size">
+      <Option name="Small" value="sm" />
+      <Option name="Medium" value="md" />
+      <Option name="Large" value="lg" />
+    </RadioButton>
+  </Container>
+);
+
+export const Label = () => (
+  <Container>
+    <RadioButton name="size" label="Size">
+      <Option name="Small" value="sm" />
+      <Option name="Medium" value="md" />
+      <Option name="Large" value="lg" />
+    </RadioButton>
+  </Container>
+);
+
+export const Sizes = () => (
+  <Container>
+    <RadioButton small name="size" label="Small">
+      <Option name="Option 1" value="1" />
+      <Option name="Option 2" value="2" />
+      <Option name="Option 3" value="3" />
+    </RadioButton>
+    <Spacer />
+    <RadioButton name="size" label="Default">
+      <Option name="Option 1" value="1" />
+      <Option name="Option 2" value="2" />
+      <Option name="Option 3" value="3" />
+    </RadioButton>
+    <Spacer />
+    <RadioButton large name="size" label="Large">
+      <Option name="Option 1" value="1" />
+      <Option name="Option 2" value="2" />
+      <Option name="Option 3" value="3" />
+    </RadioButton>
+  </Container>
+);
+
+export const reactHookForm = () => {
+  const schema = yup
+    .object({
+      size: yup.string().required('Size is required'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: any) => {
+    alert(JSON.stringify(data, null, 2));
+  };
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <RadioButton
+          {...register('size')}
+          label="Size"
+          error={get(errors, 'size')}
+        >
+          <Option name="Small" value="sm" />
+          <Option name="Medium" value="md" />
+          <Option name="Large" value="lg" />
+        </RadioButton>
+        <br />
+        <Button marginT-30>Submit</Button>
+      </form>
+    </Container>
+  );
+};
